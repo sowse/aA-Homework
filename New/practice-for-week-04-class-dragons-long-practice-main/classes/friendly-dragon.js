957e311e2137a96a5617f2d0bf0b27531cee775f
sowse <nickos1992@gmail.com>
2023-04-19 14:44:41 +0300

    Completed class practice dragons

---
+++ New/practice-for-week-04-class-dragons-long-practice-main/classes/friendly-dragon.js
@@ -0,0 +1,20 @@
+// Your code here
+const Dragon = require('./dragon.js')
+
+class FriendlyDragon extends Dragon{
+    constructor(name, color, lifeGoals=[], friend) {
+        super(name, color);
+        this.lifeGoals = lifeGoals;
+        this.friend = friend;
+    }
+
+    hasLifeGoals() {
+        this.lifeGoals.forEach(goal => console.log(`${this.name} likes to ${goal}`));
+    }
+
+    helpsPeople() {
+        return `${this.name} helps their friend ${this.friend}`
+    }
+}
+
+module.exports = FriendlyDragon;
