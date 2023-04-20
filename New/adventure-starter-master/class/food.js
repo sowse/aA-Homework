fd00273af2e0e49fee6f081abe5df811ae03c5d9
sowse <nickos1992@gmail.com>
2023-04-20 09:54:03 +0300

    Completed adventure starter project

---
+++ New/adventure-starter-master/class/food.js
@@ -0,0 +1,14 @@
+
+// FILL THIS OUT
+
+const { Item } = require("./item");
+
+class Food extends Item {
+  constructor(name, description) {
+    super(name, description);
+  }
+}
+
+module.exports = {
+  Food,
+};
