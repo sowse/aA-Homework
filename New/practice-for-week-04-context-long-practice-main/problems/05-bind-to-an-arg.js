42b843dda0ba2018dba24c4bab5869219651182f
sowse <nickos1992@gmail.com>
2023-04-25 12:15:35 +0300

    Revert "Completed context long practice"
    This reverts commit daa07fc98273b69fd2f2b2c78620d243dd969d4b.

--- New/practice-for-week-04-context-long-practice-main/problems/05-bind-to-an-arg.js
+++
@@ -1,8 +0,0 @@
-function bindToAnArg(func, arg) {
-  // Your code here
- let bound = func.bind(this, arg);
- return bound;
-}
-
-/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
-module.exports = bindToAnArg;
